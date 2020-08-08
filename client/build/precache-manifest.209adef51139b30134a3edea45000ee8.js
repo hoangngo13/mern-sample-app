ff6a6cdc6c65f74966046938827277c23b416d25
Ngo Dinh Nhat Hoang <ngo.dinh.nhat.hoang@sun-asterisk.com>
2020-08-08 21:15:38 +0700

    Un-ignore client/build

---
+++ client/build/precache-manifest.209adef51139b30134a3edea45000ee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78f2e2ac7963f86dc23818f75ccf6c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed3b17fc432100c5462c",
+    "url": "/static/css/main.24031a1f.chunk.css"
+  },
+  {
+    "revision": "307519026cab66181b9f",
+    "url": "/static/js/2.33e6cdc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33e6cdc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3b17fc432100c5462c",
+    "url": "/static/js/main.fac8d1dc.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
