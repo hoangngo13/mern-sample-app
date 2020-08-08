8e8e354ecf06e9f75211b1baae2a789a724880d4
Ngo Dinh Nhat Hoang <ngo.dinh.nhat.hoang@sun-asterisk.com>
2020-08-08 20:57:05 +0700

    Add file one more time

---
+++ config/index.js
@@ -0,0 +1,6 @@
+const dbuser = 'hoangngo01';
+const dbpassword = 'neoTCpDFG2Fl7FxH';
+
+const MONGODB_URI = "mongodb+srv://hoangngo01:neoTCpDFG2Fl7FxH@cluster0.fmxac.mongodb.net/test01?retryWrites=true&w=majority";
+
+module.exports = MONGODB_URI;
